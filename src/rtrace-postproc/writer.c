4886f0772eb0cfeaf82d40352bcb738449dacd9e
Andris Zeila <andris.zeila@accenture.com>
2010-10-01 15:39:47 +0300

    Moved allocation summary before backtrce for compressed output.

--- src/rtrace-postproc/writer.c
+++ src/rtrace-postproc/writer.c
@@ -124,9 +124,9 @@ static int write_compressed_function_call(ref_node_t* node, fmt_data_t* fmt)
 static int write_compressed_backtrace(ftrace_ref_t* trace, fmt_data_t* fmt)
 {
 	dlist_foreach2(&trace->ref->calls, (op_binary_t)write_compressed_function_call, fmt);
-	formatter_write_ftrace(trace->ref, fmt->fp);
-	formatter_printf(fmt->fp, "# allocation summary for the last backtrace: %d block(s) with total size %d\n", 
+	formatter_printf(fmt->fp, "# allocation summary: %d block(s) with total size %d\n", 
 			trace->leak_count, trace->leak_size);
+	formatter_write_ftrace(trace->ref, fmt->fp);
 	return 0;
 }
 
