0b0aee5fa6cd4bcce64ae1b51240473a9d3c353b
Tommi Rantala <ext-tommi.1.rantala@nokia.com>
2012-02-01 15:14:40 +0200

    Use backtrace() provided by libunwind
    When backtracing with libunwind, use the backtrace() function provided
    in the library. There are several benefits:
     - The libunwind support code in sp-rtrace is greatly simplified.
     - We no longer need headers from libuwind at build time, meaning that
       the sp-rtrace libunwind support will always be available.
     - backtrace() in libunwind contains optimized trace code for some
       platforms, improving performance compared to the code we have in
       sp-rtrace.
    
    The "backtrace" symbol is exported in libunwind.so, so we dlopen() it
    instead of libunwind-$(arch).so.

--- src/modules/libunwind_support.c
+++ src/modules/libunwind_support.c
@@ -27,90 +27,39 @@
 #include "libunwind_support.h"
 #include "common/debug_log.h"
 
-
-#ifdef USE_LUNWIND
-
-#define UNW_LOCAL_ONLY
-#include <libunwind.h>
-
-typedef int (*unw_init_local_t)(unw_cursor_t*, unw_context_t*);
-typedef int (*unw_step_t)(unw_cursor_t*);
-typedef int (*unw_get_reg_t)(unw_cursor_t*, unw_regnum_t, unw_word_t *);
-
-
 /* libunwind library handle */
 static void* handle = NULL;
 
-/* used libunwind functions */
-static unw_init_local_t punw_init_local = NULL;
-static unw_step_t punw_step = NULL;
-static unw_get_reg_t punw_get_reg = NULL;
-
-
-#ifdef __arm__
- #define ARCH arm
-#else
- #define ARCH x86
-#endif
-
-#define STR2(x)			#x
-#define STR(x)			STR2(x)
-
-#define LUNW_SYM(x) 	"_U" STR(ARCH) "_" #x
-#define LUNW_LIB()		"libunwind-" STR(ARCH) ".so.7.0.0"
-
-
-/**
- * Retrieves function call stack trace.
- * 
- * This function is meant as direct replacement for libc6 backtrace() 
- * function - so it has the same signature.
- * @param[out] frames  the output buffer for frame addresses.
- * @param[in] size     the maximum number of frame addresses to retrieve.
- * @return             the number of frame addresses retrieved.
- */
-static int libunwind_backtrace(void** frames, int size)
-{
-	void** ptr = frames;
-	unw_cursor_t cursor; 
-	unw_context_t uc;
-	unw_word_t ip;
-
-	if (unw_getcontext(&uc) != 0) {
-	  return 0;
-	}
-	
-	if (punw_init_local(&cursor, &uc) != 0) {
-	  return 0;
-	}
-
-	while (size-- > 0 && punw_step(&cursor) > 0) {
-	  punw_get_reg(&cursor, UNW_REG_IP, &ip);
-	  *ptr++ = (void*)ip;
-	}
-	return ptr - frames;
-}
+static const char* const libs[] = {
+	"libunwind.so.7.0.0",
+};
 
 /*
  * Public API implementation
  */
 fn_backtrace_t libunwind_initialize()
 {
-	handle = dlopen(LUNW_LIB(), RTLD_LAZY);
-	if (handle) {
-		punw_init_local = (unw_init_local_t)dlsym(handle, LUNW_SYM(init_local));
-		punw_step = (unw_step_t)dlsym(handle, LUNW_SYM(step));
-		punw_get_reg = (unw_get_reg_t)dlsym(handle, LUNW_SYM(get_reg));
-		if (!punw_init_local || !punw_step || !punw_get_reg) {
-			LOG("Failed to load libunwind symbols");
-			dlclose(handle);
-			handle = NULL;
-			return NULL;
-		}
-		return libunwind_backtrace;
+	unsigned i;
+	fn_backtrace_t bt;
+
+	for (i=0; i < sizeof(libs) / sizeof(libs[0]); ++i) {
+		handle = dlopen(libs[i], RTLD_LAZY);
+		if (handle)
+			break;
 	}
-	LOG("Failed to open %s.so library", LUNW_LIB());
-	return NULL;
+
+	if (!handle) {
+		LOG("Failed to dlopen() libunwind");
+		return NULL;
+	}
+
+	bt = dlsym(handle, "backtrace");
+	if (!bt) {
+		LOG("Failed to dlsym() 'backtrace' from libunwind");
+		libunwind_release();
+	}
+
+	return bt;
 }
 
 int libunwind_release()
@@ -121,20 +70,3 @@ int libunwind_release()
 	}
 	return 0;
 }
-
-#else
-
-fn_backtrace_t libunwind_initialize()
-{
-	LOG("Attempting to call libunwind_initialize() when libunwind was not available at compilation time");
-	return NULL;
-}
-
-int libunwind_release()
-{
-	return 0;
-}
-
-#endif
-
-
